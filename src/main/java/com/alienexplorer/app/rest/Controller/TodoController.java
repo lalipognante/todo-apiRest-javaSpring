20c641079a749fe93f1e68dc204ddeae46497733
laura.pognante <laura.pognante@proveedor.ues21.edu.ar>
2023-06-24 17:20:45 -0300

    Initial commit

---
+++ src/main/java/com/alienexplorer/app/rest/Controller/TodoController.java
@@ -0,0 +1,52 @@
+package com.alienexplorer.app.rest.Controller;
+
+import com.alienexplorer.app.rest.Model.Task;
+import com.alienexplorer.app.rest.Repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TodoController {
+     @Autowired //instancia repo en la clase
+     private  TodoRepository todoRepository;
+
+    @GetMapping(value = "/") // pagina principal
+    public String holaMundo() {
+        return "HOLA MUNDO!!";
+    }
+
+    @GetMapping(value = "/tasks")
+    public List<Task> getTasks(){
+        return todoRepository.findAll();
+    }
+
+
+    @PostMapping(value = "/savetask")
+    public String saveTask(@RequestBody Task task) {
+        todoRepository.save(task);
+        return "Saved Task";
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public String updateTask(@PathVariable long id, @RequestBody Task task){
+        // rastreamos la tarea que queremos actualizar
+        Task updatedTask = todoRepository.findById(id).get();
+        // actualizamos la tarea que estaba guardada seteandole los valores nuevos del objeto "task" que recibe el endpoint
+        updatedTask.setTitle(task.getTitle());
+        updatedTask.setDescription(task.getDescription());
+        // guardamos esta tarea actualizada
+        todoRepository.save(updatedTask);
+        // retornamos al usuario que salio con exito
+        return "Updated Task";
+    }
+
+
+    @DeleteMapping(value = "/delete/{id}")
+    public String deleteTask(@PathVariable long id){
+        Task deletedTask = todoRepository.findById(id).get();
+        todoRepository.delete(deletedTask);
+        return "Deleted Task";
+    }
+}
---
+++ src/main/java/com/alienexplorer/app/rest/Model/Task.java
@@ -0,0 +1,41 @@
+package com.alienexplorer.app.rest.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(schema = "TasksCrud")
+public class Task {
+    // definición de componentes de entidad
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // unico e autoincrementable
+    private long id;
+    @Column
+    private String title;
+    @Column
+    private String description;
+
+    // getters y setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
